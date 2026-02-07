ba9a667d96526d7034d7b13104b6a3a1fa0239a7
Salifu Malik <salifuabdulmalik69@gmail.com>
2026-02-07 16:19:08 +0000

    Add files via upload

---
+++ MovieQuote/src/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+public class MovieQuoteInfo {
+  public static void main(String[] args)
+  {
+   String movieName = "Prison Break";
+   String movieQuote = "A safe place in haven";
+   String movieCharacter = "Michael Scofield";
+   int movieYear = 2005;
+    System.out.println("The name of the  movie  is "+movieName+ " and the favourite quote is \""+ movieQuote+ "\" which was said by "+movieCharacter+ "  in the year "+movieYear+".");
+
+
+  }
+}
