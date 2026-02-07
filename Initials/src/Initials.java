9a4087f6d589c9ab8be284aa19a2c2bcf0feb9bf
Salifu Malik <salifuabdulmalik69@gmail.com>
2026-02-07 16:16:13 +0000

    Add files via upload

---
+++ Initials/src/Initials.java
@@ -0,0 +1,8 @@
+public class Initials {
+    public static void main(String[] args) {
+        char Janet = 'J';
+        char Malik = 'M';
+        char Family = 'F';
+        System.out.println(Janet + "." + Malik + "." + Family + ".");
+    }
+}
