7424d9846a435c933e7b3dd1b8cb33e219849581
Salifu Malik <salifuabdulmalik69@gmail.com>
2026-02-07 16:24:21 +0000

    Add files via upload

---
+++ RandomGuessMatch/src/RandomGuessMatch.java
@@ -0,0 +1,31 @@
+/**
+  Write a program that selects a random number between 1 and 5 and asks
+  the user to guess the number. Display a message that indicates the difference
+  between the random number and the user’s guess. Display another message that
+  displays the random number and the Boolean value true or false depending
+  on whether the user’s guess equals the random number. Save the file as
+  RandomGuessMatch.java.
+ */
+
+import java.util.Scanner;
+public class RandomGuessMatch {
+    public static void main(String[] args) {
+        int random, userEntry;
+        boolean results;
+
+        Scanner inputDevice = new Scanner(System.in);
+        System.out.println("Enter any any number between 1 and 5 inclusive:");
+        userEntry = inputDevice.nextInt();
+
+        random = 1 + (int)(Math.random() * 5);
+        results = random == userEntry;
+
+        System.out.println(" You entered "+ userEntry + " and  its "+ results + " because the random number chosen is "+random );
+
+    }
+
+
+
+
+
+}
