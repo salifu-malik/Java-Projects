eb92ca93c14e62f4397be5671ca4d9cd0679fb4a
salifu-malik <salifuabdulmalik69@gmail.com>
2026-02-09 14:58:19 +0000

    First Springbioot Web Project

---
+++ firstWebProject/src/main/java/com/mankind/firstWebProject/FirstWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mankind.firstWebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebProjectApplication.class, args);
+	}
+
+}
---
+++ firstWebProject/src/test/java/com/mankind/firstWebProject/FirstWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mankind.firstWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
