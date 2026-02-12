3382e0ae2c0533f66673dfbbd3e17b94cc6bfe8a
salifu-malik <salifuabdulmalik69@gmail.com>
2026-02-12 09:28:36 +0000

    Hospital Management System

---
+++ HospitalManagement/src/main/java/com/mankind/HospitalManagement/HospitalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mankind.HospitalManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementApplication {
+
+	void main(String[] args) {
+		SpringApplication.run(HospitalManagementApplication.class, args);
+	}
+
+}
