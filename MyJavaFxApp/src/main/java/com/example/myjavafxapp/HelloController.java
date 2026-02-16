c99a4822648f0c9a04ee55a229c33cd7aa454846
Salifu Malik <salifuabdulmalik69@gmail.com>
2026-02-16 19:22:54 +0000

    Update HelloController.java

--- MyJavaFxApp/src/main/java/com/example/myjavafxapp/HelloController.java
+++ MyJavaFxApp/src/main/java/com/example/myjavafxapp/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to JavaFX App!");
     }
 
 
